3bad00cb9073f54cf2c60df53e188f0e5f2d5a04
Yong Woo Cho <ywc8851@naver.com>
2024-04-29 13:21:02 +0900

    feat: route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Landing from "@pages/Landing/index.tsx";
+import GlobalStyles from "./styles/GlobalStyled.ts";
+
 function App() {
-	return <>app</>;
+	return (
+		<Router>
+			<AppContainer>
+				<GlobalStyles />
+				<Routes>
+					<Route path="/" element={<Landing />} />
+				</Routes>
+			</AppContainer>
+		</Router>
+	);
 }
 
 export default App;
+
+const AppContainer = styled.div`
+	width: 100vw;
+	max-width: 700px;
+	min-height: 100vh;
+	padding: 0;
+	margin: 0;
+	background-color: white;
+`;
