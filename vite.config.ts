3c4fd4ac7efd5bcdb234e887840799c05e95e111
Yong Woo Cho <ywc8851@naver.com>
2024-04-30 14:43:23 +0900

    chore: vite config tsconfigPaths 추가 및 절대경로 세팅 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default () => {
 	return defineConfig({
-		plugins: [svgr(), react()],
-		resolve: {
-			alias: {
-				"@pages": resolve(__dirname, "src/pages"),
-				"@components": resolve(__dirname, "src/components"),
-				"@styles": resolve(__dirname, "src/styles"),
-				"@store": resolve(__dirname, "src/store"),
-				"@hooks": resolve(__dirname, "src/hooks"),
-				"@type": resolve(__dirname, "src/type"),
-				"@mocks": resolve(__dirname, "src/mocks"),
-			},
-		},
+		plugins: [svgr(), react(), tsconfigPaths()],
+
 		server: {
 			proxy: {
 				// proxy 관련 설정
