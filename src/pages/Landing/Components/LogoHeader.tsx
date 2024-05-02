f7967e797b707ea23347be5140422170564a1790
Yong Woo Cho <ywc8851@naver.com>
2024-05-03 08:40:13 +0900

    feat: App header 임시구현

---
+++ src/pages/Landing/Components/LogoHeader.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const LogoHeader = () => {
+	return <Container>로고</Container>;
+};
+export default LogoHeader;
+
+const Container = styled.header`
+	width: 100%;
+	height: 52px;
+	padding: 0 20px;
+	position: fixed;
+	top: 0;
+	max-width: 360px;
+	background-color: red;
+`;
