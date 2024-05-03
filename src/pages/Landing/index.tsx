3c4d92ce603274791ab4cff17e3c7bf0b8350de9
Yong Woo Cho <ywc8851@naver.com>
2024-05-03 15:19:43 +0900

    feat: 랜딩페이지 layout 구현

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+import ServiceIntroduce from "./Components/ServiceIntroduce";
+import LogoHeader from "./Components/LogoHeader";
+import YoutubeToday from "./Components/YoutubeToday";
+const index = () => {
+	return (
+		<Container>
+			<LogoHeader />
+			<ServiceIntroduce />
+			<GoogleLoginBtn>구글 계정 연동해서 무료 구독하기</GoogleLoginBtn>
+			<YoutubeToday />
+		</Container>
+	);
+};
+
+export default index;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	font-family: "Pretendard Variable";
+	padding-top: 76px;
+`;
+
+const GoogleLoginBtn = styled.button`
+	width: 320px;
+	height: 60px;
+	border-radius: 4px;
+	background-color: rgba(0, 123, 255, 1);
+	margin-bottom: 36px;
+
+	font-family: inherit;
+	font-size: 16px;
+	font-weight: 700;
+	line-height: 22px;
+	text-align: center;
+	color: rgba(255, 255, 255, 1);
+`;
