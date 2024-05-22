3be1617c14c42d58cb68dc1df42a36e483c5925e
yongwoo_cho <ywc8851@naver.com>
2024-05-22 21:38:53 +0900

    feat: 구글 로그인 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<GoogleOAuthProvider clientId={CLIENT_ID}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</GoogleOAuthProvider>
 );
---
+++ src/pages/Landing/Components/GoogleLoginBtn.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+
+interface GoogleDecodedProps {
+	aud: string;
+	azp: string;
+	email: string;
+	email_varified: boolean;
+	exp: number;
+	family_name: string;
+	given_name: string;
+	iat: number;
+	iss: string;
+	jti: string;
+	name: string;
+	nbf: number;
+	picture: string;
+	sub: string;
+}
+
+const GoogleLoginBtn = () => {
+	const onGoogleLogin = async (res: CredentialResponse) => {
+		const decoded: GoogleDecodedProps = jwtDecode(JSON.stringify(res));
+
+		console.log(decoded);
+	};
+
+	return (
+		<GoogleLogin
+			onSuccess={(credentialResponse) => {
+				onGoogleLogin(credentialResponse);
+			}}
+			onError={() => console.log("로그인 실패")}
+		></GoogleLogin>
+	);
+};
+
+export default GoogleLoginBtn;
