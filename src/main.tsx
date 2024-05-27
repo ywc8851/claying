948fec621c69652bb8620a8a1c49c4729a36d241
Yong Woo Cho <ywc8851@naver.com>
2024-05-27 14:48:48 +0900

    chore: recoilroot 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { RecoilRoot } from "recoil";
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<GoogleOAuthProvider clientId={CLIENT_ID}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<RecoilRoot>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</RecoilRoot>
 	</GoogleOAuthProvider>
 );
