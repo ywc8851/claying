73005256e7aa9bfcaa88e40e0e079f9aa47dbe46
Yong Woo Cho <ywc8851@naver.com>
2024-06-04 15:17:17 +0900

    config: 절대경로 관련 파일수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default () => {
 	return defineConfig({
 		plugins: [svgr(), react(), tsconfigPaths()],
-
+		base: "./",
+		resolve: {
+			alias: [
+				{ find: "@", replacement: resolve(__dirname, "src") },
+				{ find: "src", replacement: resolve(__dirname, "src/") },
+			],
+		},
 		server: {
 			proxy: {
 				// proxy 관련 설정
