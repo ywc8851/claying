38613a27aa1710eec795f147015563b3fd1de55f
Yong Woo Cho <ywc8851@naver.com>
2024-06-11 15:37:48 +0900

    feat: 상단 헤더 기능 구현

--- src/pages/Landing/Components/LogoHeader.tsx
+++ src/pages/Landing/Components/LogoHeader.tsx
@@ -2,10 +2,27 @@ import styled from "styled-components";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { userState } from "@/store/user";
 import { googleLogout } from "@react-oauth/google";
+import BackIcon from "@/assets/back.svg?react";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const LogoHeader = () => {
+interface LogoHeaderProps {
+	title?: string;
+}
+
+const LogoHeader = ({ title }: LogoHeaderProps) => {
 	const user = useRecoilValue(userState);
 	const setUser = useSetRecoilState(userState);
+	const navigate = useNavigate();
+	const location = useLocation();
+	const isDetailPage = location.pathname.includes("/detail");
+
+	const goBack = () => {
+		navigate(-1);
+	};
+
+	const goHome = () => {
+		navigate("/");
+	};
 
 	const logOut = () => {
 		googleLogout();
@@ -18,7 +35,9 @@ const LogoHeader = () => {
 
 	return (
 		<Container>
-			로고
+			{isDetailPage && <BackIcon onClick={goBack} />}
+			<span onClick={goHome}>로고</span>
+			{title && <Title>{title}</Title>}
 			<ProfileImage onClick={logOut}>
 				{user.picture !== "" && <img src={user.picture} alt="User profile" />}
 			</ProfileImage>
@@ -34,12 +53,24 @@ const Container = styled.header`
 	position: fixed;
 	top: 0;
 	max-width: 360px;
-	background-color: red;
+	background-color: rgba(244, 244, 244, 1);
+	font-family: "Pretendard Variable";
 
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
 	z-index: 1000;
+`;
+
+const Title = styled.span`
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 19.09px;
+
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	max-width: 60%;
 `;
 
 const ProfileImage = styled.div`
