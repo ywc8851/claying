85075afe50913a152e77869a99eac4d7a45100e5
yongwoo_cho <ywc8851@naver.com>
2024-06-18 20:45:33 +0900

    style: 구글 로그인 버튼 css 추가

--- src/pages/Landing/Components/GoogleLoginBtn.tsx
+++ src/pages/Landing/Components/GoogleLoginBtn.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useSetRecoilState } from "recoil";
 import { userState } from "@/store/user";
 import { auth } from "@/firebase";
@@ -8,7 +9,7 @@ const GoogleLoginBtn = () => {
 
 	const provider = new GoogleAuthProvider();
 
-	const signInGoole = async () => {
+	const signInGoogle = async () => {
 		try {
 			const { user } = await signInWithPopup(auth, provider);
 			setUser({
@@ -21,7 +22,20 @@ const GoogleLoginBtn = () => {
 		}
 	};
 
-	return <button onClick={signInGoole}>구글로 로그인 하기</button>;
+	return <GoogleLoginButton onClick={signInGoogle}>구글 계정 연동해서 무료 구독하기</GoogleLoginButton>;
 };
 
 export default GoogleLoginBtn;
+
+const GoogleLoginButton = styled.button`
+	width: 320px;
+	height: 60px;
+	border-radius: 4px;
+	background-color: #007bff;
+
+	font-family: "Pretendard Variable";
+	font-size: 16px;
+	font-weight: 700;
+	line-height: 22px;
+	color: white;
+`;
