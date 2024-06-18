b9c048f22d9c18f14b83dafa61568f27bafafb24
Yong Woo Cho <ywc8851@naver.com>
2024-06-18 17:19:44 +0900

    chore: @react-oauth/google 관련 코드 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { RecoilRoot } from "recoil";
 
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<GoogleOAuthProvider clientId={CLIENT_ID}>
-		<RecoilRoot>
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		</RecoilRoot>
-	</GoogleOAuthProvider>
+	<RecoilRoot>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</RecoilRoot>
 );
