f894a5b0e5bfa69bd809fe4ae2c06b1bda315c6c
Yong Woo Cho <ywc8851@naver.com>
2024-07-12 13:50:58 +0900

    feat: api 연동

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,17 +1,38 @@
+import { useEffect } from "react";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 import { styled } from "styled-components";
+import axios from "axios";
 import ServiceIntroduce from "./Components/ServiceIntroduce";
 import LogoHeader from "./Components/LogoHeader";
 import YoutubeToday from "./Components/YoutubeToday";
 import Footer from "./Components/Footer";
 import GoogleLoginBtn from "./Components/GoogleLoginBtn";
+import { dataState } from "@/store/data";
 
 const index = () => {
+	const setApiData = useSetRecoilState(dataState);
+	const apiData = useRecoilValue(dataState);
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await axios.get("https://uncrapi.link/briefing/top_videos/");
+				setApiData(response.data);
+				console.log(response.data[0]);
+			} catch (error) {
+				console.error("Error fetching top videos:", error);
+			}
+		};
+
+		getData();
+	}, [setApiData]);
+
 	return (
 		<Container>
 			<LogoHeader />
 			<ServiceIntroduce />
 			<GoogleLoginBtn />
-			<YoutubeToday />
+			<YoutubeToday data={apiData} />
 			<Footer />
 		</Container>
 	);
