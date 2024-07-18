84d5606374cb218f5034cf79c7df4d9b8c3cb2b8
Yong Woo Cho <ywc8851@naver.com>
2024-07-18 14:15:04 +0900

    chore: 주제 data 파일로 분리

---
+++ src/constants/topic.tsx
@@ -0,0 +1,81 @@
+import AllIcon from "@/assets/all.svg?react";
+import ChartIcon from "@/assets/chart.svg?react";
+import HouseIcon from "@/assets/house.svg?react";
+import CoinIcon from "@/assets/coin.svg?react";
+import BeautyIcon from "@/assets/beauty.svg?react";
+import EconomyIcon from "@/assets/economy.svg?react";
+
+export const YOUTUBE_TOPICS = [
+	{
+		topic: "전체",
+		icon: <AllIcon />,
+	},
+	{
+		topic: "주식",
+		icon: <ChartIcon />,
+	},
+	{
+		topic: "부동산",
+		icon: <HouseIcon />,
+	},
+	{
+		topic: "가상자산",
+		icon: <CoinIcon />,
+	},
+	{
+		topic: "뷰티/메이크업",
+		icon: <BeautyIcon />,
+	},
+	{
+		topic: "경제",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "정치",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "과학",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "IT/테크",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "자동차",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "건강",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "자기계발",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "요리",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "연애/결혼",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "패션",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "피트니스",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "인공지능",
+		icon: <EconomyIcon />,
+	},
+	{
+		topic: "역사",
+		icon: <EconomyIcon />,
+	},
+];
