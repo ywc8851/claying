9295d3e28ac92b457e26606d6c8a6adef100febe
Yong Woo Cho <ywc8851@naver.com>
2024-07-22 15:13:03 +0900

    chore: 주제 관련 데이터 분리

--- src/constants/topic.tsx
+++ src/constants/topic.tsx
@@ -2,8 +2,21 @@ import AllIcon from "@/assets/all.svg?react";
 import ChartIcon from "@/assets/chart.svg?react";
 import HouseIcon from "@/assets/house.svg?react";
 import CoinIcon from "@/assets/coin.svg?react";
-import BeautyIcon from "@/assets/beauty.svg?react";
 import EconomyIcon from "@/assets/economy.svg?react";
+import HealthIcon from "@/assets/health.svg?react";
+import ScienceIcon from "@/assets/science.svg?react";
+import ItTechIcon from "@/assets/itTech.svg?react";
+import CarIcon from "@/assets/car.svg?react";
+import DevelopIcon from "@/assets/develop.svg?react";
+import CookIcon from "@/assets/cook.svg?react";
+import MarriageIcon from "@/assets/marriage.svg?react";
+import ManFashionIcon from "@/assets/manFashion.svg?react";
+import WomanFashionIcon from "@/assets/womanFashion.svg?react";
+import FitnessIcon from "@/assets/fitness.svg?react";
+import TripIcon from "@/assets/trip.svg?react";
+import AiIcon from "@/assets/ai.svg?react";
+import HistoryIcon from "@/assets/history.svg?react";
+import BeautyIcon from "@/assets/beauty.svg?react";
 
 export const YOUTUBE_TOPICS = [
 	{
@@ -23,12 +36,12 @@ export const YOUTUBE_TOPICS = [
 		icon: <CoinIcon />,
 	},
 	{
-		topic: "뷰티/메이크업",
-		icon: <BeautyIcon />,
-	},
-	{
 		topic: "경제",
 		icon: <EconomyIcon />,
+	},
+	{
+		topic: "건강",
+		icon: <HealthIcon />,
 	},
 	{
 		topic: "정치",
@@ -36,46 +49,56 @@ export const YOUTUBE_TOPICS = [
 	},
 	{
 		topic: "과학",
-		icon: <EconomyIcon />,
+		icon: <ScienceIcon />,
 	},
 	{
 		topic: "IT/테크",
-		icon: <EconomyIcon />,
+		icon: <ItTechIcon />,
 	},
 	{
 		topic: "자동차",
-		icon: <EconomyIcon />,
-	},
-	{
-		topic: "건강",
-		icon: <EconomyIcon />,
+		icon: <CarIcon />,
 	},
 	{
 		topic: "자기계발",
-		icon: <EconomyIcon />,
+		icon: <DevelopIcon />,
 	},
 	{
 		topic: "요리",
-		icon: <EconomyIcon />,
+		icon: <CookIcon />,
 	},
 	{
 		topic: "연애/결혼",
-		icon: <EconomyIcon />,
+		icon: <MarriageIcon />,
 	},
 	{
-		topic: "패션",
-		icon: <EconomyIcon />,
+		topic: "남자 패션",
+		icon: <ManFashionIcon />,
+	},
+	{
+		topic: "여자 패션",
+		icon: <WomanFashionIcon />,
 	},
 	{
 		topic: "피트니스",
-		icon: <EconomyIcon />,
+		icon: <FitnessIcon />,
+	},
+	{
+		topic: "여행",
+		icon: <TripIcon />,
 	},
 	{
 		topic: "인공지능",
-		icon: <EconomyIcon />,
+		icon: <AiIcon />,
 	},
 	{
 		topic: "역사",
-		icon: <EconomyIcon />,
+		icon: <HistoryIcon />,
+	},
+	{
+		topic: "뷰티/메이크업",
+		icon: <BeautyIcon />,
 	},
 ];
+
+export const TOPIC_TAGS = YOUTUBE_TOPICS.filter(({ topic }) => topic !== "전체").map(({ topic }) => topic);
