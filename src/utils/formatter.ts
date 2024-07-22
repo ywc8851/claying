7634122f2f257d235f540fc1a2485c2c2eb06586
Yong Woo Cho <ywc8851@naver.com>
2024-07-22 15:14:02 +0900

    chore: util 관련 함수 파일로 분리

---
+++ src/utils/formatter.ts
@@ -0,0 +1,27 @@
+export const parseSubscribersCount = (count: number): string => {
+	if (count < 1000) {
+		return `${count}명`;
+	} else if (count < 10000) {
+		return `${(count / 1000).toFixed(2).replace(/\.?0+$/, "")}천명`;
+	} else if (count < 100000000) {
+		if (count >= 1000000) {
+			return `${Math.floor(count / 10000)}만명`;
+		}
+		return `${(count / 10000).toFixed(2).replace(/\.?0+$/, "")}만명`;
+	} else {
+		return `${(count / 100000000).toFixed(1).replace(/\.?0+$/, "")}억명`;
+	}
+};
+
+const formatToTwoDigits = (time: number): string => time.toString().padStart(2, "0");
+
+export const calcuateTimeLeft = (): string => {
+	const now = new Date();
+	const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 9, 0, 0);
+	const diff = tomorrow.getTime() - now.getTime();
+	const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+	const minutes = Math.floor((diff / (1000 * 60)) % 60);
+	const seconds = Math.floor((diff / 1000) % 60);
+
+	return `${formatToTwoDigits(hours)}:${formatToTwoDigits(minutes)}:${formatToTwoDigits(seconds)}`;
+};
