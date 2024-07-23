d4282feb300c75faaa6c3c22f113fa22e702e2d1
Yong Woo Cho <ywc8851@naver.com>
2024-07-23 16:21:41 +0900

    chore: util 함수 수정

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -25,3 +25,16 @@ export const calcuateTimeLeft = (): string => {
 
 	return `${formatToTwoDigits(hours)}:${formatToTwoDigits(minutes)}:${formatToTwoDigits(seconds)}`;
 };
+
+const formatMinutesToTime = (minutes: number): string => {
+	const hours = Math.floor(minutes / 60);
+	const remainingMinutes = minutes % 60;
+	const formattedHours = hours.toString().padStart(2, "0");
+	const formattedMinutes = remainingMinutes.toString().padStart(2, "0");
+	return `${formattedHours}:${formattedMinutes}`;
+};
+
+export const formatTimeRange = (startMinutes: number): string => {
+	const startTime = formatMinutesToTime(startMinutes);
+	return `${startTime} ~`;
+};
