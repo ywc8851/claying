2329da35763d85776b19f2a04e43e20519350cef
Yong Woo Cho <ywc8851@naver.com>
2024-07-30 09:03:43 +0900

    feat: 학준님 code merge

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -181,23 +181,16 @@ const TOC = styled.div`
 	}
 
 	div:nth-child(2) {
-		padding: 12px 16px 12px 16px;
+		padding: 20px;
 		display: flex;
 		flex-direction: column;
-		gap: 12px;
+		gap: 24px;
 		background-color: rgba(242, 242, 242, 1);
-		font-size: 16px;
+		font-size: 18px;
 		font-weight: 600;
 		line-height: 19.09px;
 	}
 `;
-
-// const Contents = styled.div`
-// 	display: flex;
-// 	flex-direction: column;
-// 	align-items: center;
-// 	padding: 0 20px;
-// `;
 
 const VideoContainer = styled.div<{ $isFixed: boolean }>`
 	position: ${(props) => (props.$isFixed ? "fixed" : "static")};
