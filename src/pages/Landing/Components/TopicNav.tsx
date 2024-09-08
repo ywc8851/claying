3ab2849774c247f568952cdc067d388bd5b67327
ywc8851 <ywc8851@naver.com>
2024-09-08 21:24:30 +0900

    style: topicNav z-index 제거

--- src/pages/Landing/Components/TopicNav.tsx
+++ src/pages/Landing/Components/TopicNav.tsx
@@ -74,7 +74,7 @@ const Container = styled.div<{ $isDesktop: boolean; $isFixed: boolean; $hasScrol
 
 	position: ${(props) => (props.$isFixed ? "fixed" : "static")};
 	top: ${(props) => (props.$isFixed ? "52px" : "auto")};
-	z-index: ${(props) => (props.$isFixed ? 10000 : 0)};
+	/* z-index: ${(props) => (props.$isFixed ? 0 : 0)}; */
 
 	overflow-x: scroll;
 	::-webkit-scrollbar {
