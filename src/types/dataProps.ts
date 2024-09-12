ae5652b97bfcf14dbade2319a47f95dbe23fe617
Yong Woo Cho <ywc8851@naver.com>
2024-09-12 16:37:14 +0900

    feat: api dataProps 수정 및 detailDataProps 추가

--- src/types/dataProps.ts
+++ src/types/dataProps.ts
@@ -1,10 +1,25 @@
 export interface DataProps {
-	id: string;
+	video_id: string;
 	section: string;
+	summary_data: {
+		headline_title: string;
+		headline_sub_title: string;
+		short_summary: string;
+	};
+	channel_details: {
+		channel_id: string;
+		channel_name: string;
+		channel_subscribers: number;
+		channel_video_count: number;
+		channel_view_count: number;
+		channel_thumbnail: string;
+		channel_banner: string;
+
+		section: TemplateSummary[];
+	};
+	score: number;
+	// 주석 위쪽이 24-09-12 version
 	template_summary: TemplateSummary[];
-	headline_title: string;
-	headline_subtitle: string;
-	short_summary: string;
 	title: string;
 	upload_date: string;
 	duration: string;
@@ -26,8 +41,54 @@ export interface DataProps {
 
 interface TemplateSummary {
 	title: string;
+	start_time: string;
 	detail_contents: string;
 	explanation_keyword: string;
 	explanation_description: string;
+}
+
+export interface DetailDataProps {
+	video_id: string;
+	title: string;
+	section: string;
+	upload_date: string;
+	duration: string;
+	thumbnail: string;
+	views: number;
+	likes: number;
+	comments: number;
+	subscribers: number;
+	score: number;
+	summary_data: SummaryData;
+	channel_details: ChannelDetails;
+}
+
+interface SummaryData {
+	headline_title: string;
+	headline_sub_title: string;
+	short_summary: string;
+	section: Section[];
+	views: number;
+	likes: number;
+	comments: number;
+	subscribers: number;
+	score: number;
+}
+
+interface Section {
+	title: string;
+	detail_contents: string;
 	start_time: string;
+	explanation_keyword: string;
+	explanation_description: string;
+}
+
+interface ChannelDetails {
+	channel_id: string;
+	channel_name: string;
+	channel_subscribers: number;
+	channel_video_count: number;
+	channel_view_count: number;
+	channel_thumbnail: string;
+	channel_banner: string;
 }
